8ab0bc80688f0808b8a8e18234f0fe1ceb6d6051
Mateusz <matbochynski@gmail.com>
2018-05-21 11:18:29 +0200

    Add routing between recipes and shopping list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() linkClicked = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipesLinkClicked() {
+    this.linkClicked.emit('recipes');
+  }
+
+  onShoppingLinkClicked() {
+    this.linkClicked.emit('shoppingList');
   }
 
 }
