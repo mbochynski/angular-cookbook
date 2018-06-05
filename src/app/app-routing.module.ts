0069d5ebd08ce7817b3f7a91ef032e77740efbe6
Mateusz <matbochynski@gmail.com>
2018-06-05 10:25:09 +0200

    Change preload startegy for lazy load modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
@@ -10,7 +10,10 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(
-      routes
+      routes,
+      {
+        preloadingStrategy: PreloadAllModules,
+      },
     ),
   ],
   exports: [
