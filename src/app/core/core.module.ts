7681964533d6583eeabd4cd88a87662d3d16dc3d
Mateusz <matbochynski@gmail.com>
2018-06-05 14:37:35 +0200

    Add interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,6 +7,8 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { RecipeService } from "../recipes/recipe.service";
 import { StorageService } from "../shared/storage.service";
 import { AuthService } from "../auth/auth.service";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "../shared/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -26,6 +28,11 @@ import { AuthService } from "../auth/auth.service";
     RecipeService, 
     StorageService, 
     AuthService, 
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
 })
 export class CoreModule {}
