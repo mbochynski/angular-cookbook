1aee0637f49d406f38a30cc7e944d6e6ab0544c6
Mateusz <matbochynski@gmail.com>
2018-06-07 14:16:47 +0200

    Export state type interface

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -4,6 +4,7 @@ import { RecipeService } from '../recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions'
+import * as fromShoppingList from '../../shopping-list/store/shopping-list.reducers';
 import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
@@ -18,7 +19,7 @@ export class RecipeDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute,
               private router: Router,
               private recipeService: RecipeService,
-              private store: Store<{ shoppingList: { ingredients: Ingredient[] }}>) {}
+              private store: Store<fromShoppingList.AppState>) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,6 +3,7 @@ import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Subscription, Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import * as fromShoppingList from './store/shopping-list.reducers';
 
 @Component({
   selector: 'app-shopping-list',
@@ -14,7 +15,7 @@ export class ShoppingListComponent implements OnInit {
   shoppingListState: Observable<{ ingredients: Ingredient[] }>;
 
   constructor(private shoppingListService: ShoppingListService,
-              private store: Store<{ shoppingList: { ingredients: Ingredient[] }}>) {
+              private store: Store<fromShoppingList.AppState>) {
   }
 
   ngOnInit() {
