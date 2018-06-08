064365728d3d0d080a27bee0db92fd4e41b86ff9
Mateusz <matbochynski@gmail.com>
2018-06-08 14:23:57 +0200

    Add auth reducers

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,3 +1,5 @@
+import * as AuthActions from './auth.actions';
+
 export interface State {
   token: string,
   authenticated: boolean,
@@ -8,6 +10,21 @@ const initialState: State = {
   authenticated: false,
 }
 
-export function authReducer(state = initialState, action) {
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case (AuthActions.SIGNUP):
+    case (AuthActions.SIGNIN):
+      return {
+        ...state,
+        authenticated: true,
+      }
+    case (AuthActions.LOGOUT):
+      return {
+        ...state,
+        authenticated: false,
+        token: null,
+      }
+  }
+
   return state;
 }
